daeea8e11115ea0234a77a24cb0ec50266911d72
Bernardo <bhernandez@consistent.com.mx>
2018-11-21 19:34:28 -0600

    creacion de vistas

--- src/main/java/com/consistent/app/controller/HomeController.java
+++ src/main/java/com/consistent/app/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.consistent.app.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +19,26 @@ public class HomeController {
 		String titulo = "Bernardo";
 		model.addAttribute("titulo", titulo);
 		return "home";
+	}
+	
+	@RequestMapping( value="/", method=RequestMethod.GET)
+	public String mostrarPrincipal(){
+		List<String> peliculas = new LinkedList<String>();
+		peliculas.add("Rapido y furioso");
+		peliculas.add("El aro 2");
+		peliculas.add("Aliens");
+		return "home";
+	}
+	
+	@RequestMapping(value = "/detail")
+	public String mostrarDetalle(Model model){
+		String tituloPelicula = "Rapidos y furioso";
+		int duracion = 136;
+		double precioEntrada = 50;
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precioEntrada);
+		return "detail";
+		
 	}
 }
---
+++ webapp/WEB-INF/views/detail.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>detail</title>
+</head>
+<body>
+<header>
+	<hgroup>
+		<h1>Detail</h1>
+	<hgroup>
+</header>
+<nav>
+</nav>
+<section>
+	<article>
+	<hgroup>
+		<c:set var = "salary" scope = "session" value = "${2000*2}"/>
+      <c:if test = "${salary > 2000}">
+         <p>My salary is:  <c:out value = "${salary}"/><p>
+      </c:if>
+		
+	</hgroup>
+	</article>
+</section>
+
+</body>
+</html>
